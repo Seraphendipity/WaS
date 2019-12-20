3ae9a8442e8f982a9387779b0088dedd45d3a0ca
Seraphendipity <seria602@gmail.com>
2019-12-20 12:27:37 -0600

    Merge Reece Assets

---
+++ WaS-Playground/Assets/Reece-Assets/Scripts/ItemUI.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class ItemUIEvent : UnityEvent<ItemUI> {}
+
+public class ItemUI : MonoBehaviour
+{
+    public Item item;
+    public Text itemName;
+    public ItemUIEvent primaryAction;
+    public ItemUIEvent secondaryAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (item != null) {
+            Display(item);
+        }
+    }
+
+    public void Display(Item item)
+    {
+        this.item = item;
+        this.itemName.text = this.item.name;
+    }
+    
+    /// <summary>
+    /// Upon user Inputting the primary key (Left Click) on this ItemUI,
+    /// perform the primary action relevant to the context.
+    /// </summary>
+    /// <remark>
+    /// In a shop, the ShopInventoryUI will sell the item and move it
+    /// it to the player's inventory.
+    /// </remark>
+    public void onPrimary()
+    {
+        primaryAction.Invoke(this);
+    }
+    public void onSecondary()
+    {
+        secondaryAction.Invoke(this);
+    }
+}
---
+++ WaS-Playground/Assets/Reece-Assets/Scripts/PlayerMovementHandler.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementHandler : MonoBehaviour
+{
+    /// <summary>
+    /// Methods applied to this allow motion.
+    /// </summary>
+    public Rigidbody me; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        me = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Provides slight acceleration boost on key down.
+        if (Input.GetButtonDown("Forward")) {
+            me.AddRelativeForce(0, 0, 5, ForceMode.Impulse);
+        }
+
+        if (Input.GetButton("Forward")) {
+            me.AddRelativeForce(0, 0, 10, ForceMode.Acceleration);
+        } 
+
+        // Provides slight acceleration boost on key down.
+        if (Input.GetButtonDown("Backward")) {
+            me.AddRelativeForce(0, 0, -5, ForceMode.Impulse);
+        }
+
+        if (Input.GetButton("Backward")) {
+            me.AddRelativeForce(0, 0, -10, ForceMode.Acceleration);
+        } 
+
+        if (Input.GetButton("Left")) {
+            me.AddRelativeTorque(0, -5, 0, ForceMode.VelocityChange);
+        } 
+
+        if (Input.GetButton("Right")) {
+            me.AddRelativeTorque(0, 5, 0, ForceMode.VelocityChange);
+       }
+    }
+}
